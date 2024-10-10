8780e17b55f580f8da0d987d2becbb020f44a891
allisonwinder <allisonwinder1@gmail.com>
2024-10-10 08:58:13 -0600

    Initial Commit

---
+++ Learn Portuguese/Learn_PortugueseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_PortugueseApp.swift
+//  Learn Portuguese
+//
+//  Created by IS 543 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_PortugueseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
